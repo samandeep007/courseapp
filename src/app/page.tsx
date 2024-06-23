07b452fc25bb8723a538de96a9a9c6136baa8915
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-22 22:22:47 -0600

    refactor: Update FeaturedCourses import in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
-import FeaturedCourses from "@/components/FeaturedCourses.tsx";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function Home() {
   return (
