30c666b91bf0defc25bcde88d32149ca91f4435e
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-23 01:39:29 -0600

    feat: Add support for lazy loading images from Pexels and Unsplash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +24,7 @@ export default function RootLayout({
       <Navbar/>
       </div>
         {children}
-    
+    <Footer/>
         </body>
     </html>
   );
