1d4f0ef2c5f59d3409e875b20173957081b76842
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-22 20:59:18 -0600

    refactor: Update HeroSection component imports and formatting

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -30,7 +30,7 @@ export default function HeroSection() {
 
                 <h1 className="mt-20 md:mt-0 text-4xl md:text-8xl md:text-wrap font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the Art of Music</h1>
 
-                <p className="mt-4 font-normal text-base md:text-2xl text-neutral-300 max-w-4xl mx-auto">Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+                <p className="mt-4 font-normal text-base md:text-xl text-neutral-300 max-w-2xl mx-auto">Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
 
                 <div className="mt-4">
                     <Link href={"/courses"}>
