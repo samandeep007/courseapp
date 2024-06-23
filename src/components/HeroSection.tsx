2094dc15cdb783763500cf4092e4b08879730e8c
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-22 21:11:36 -0600

    refactor: Update HeroSection component font size and text content

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -28,7 +28,7 @@ export default function HeroSection() {
 
             <div className="p-4 relative z-10 w-full text-center">
 
-                <h1 className="mt-20 md:mt-0 text-4xl md:text-8xl md:text-wrap font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the Art of Music</h1>
+                <h1 className="mt-20 md:mt-0 text-7xl md:text-8xl md:text-wrap font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the Art of Music</h1>
 
                 <p className="mt-4 font-normal text-base md:text-xl text-neutral-300 max-w-2xl mx-auto">Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
 
